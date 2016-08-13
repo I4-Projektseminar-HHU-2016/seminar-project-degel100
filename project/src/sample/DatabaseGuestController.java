f7693009e17887168b47fbc582bf1a87784bdb33
Gelfart, Dennis (degel100) <dennis.gelfart@uni-duesseldorf.de>
2016-08-13 12:42:08 +0200

    add gui for the database when entering as a guest

---
+++ project/src/sample/DatabaseGuestController.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by dennisgelfart on 13.08.16.
+ */
+public class DatabaseGuestController implements Initializable{
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    private TextField searchField;
+
+
+}
--- project/src/sample/LoginController.java
+++ project/src/sample/LoginController.java
@@ -1,9 +1,15 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,6 +28,25 @@ public class LoginController implements Initializable {
 
     @FXML
     private TextField passwordField;
+
+
+
+    //Gast Button
+    public void Guest (ActionEvent event){
+
+        try {
+            Stage primaryStage = new Stage();
+            FXMLLoader loader = new FXMLLoader();
+            Pane root = loader.load(getClass().getResource("DatabaseGuest.fxml").openStream());
+            Scene scene = new Scene(root);
+
+            primaryStage.setTitle("Olympische Spieler 2016 - Datenbank");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
